ea2b3e7e9bda3e5570fc7112a4f4502dfbd8e925
ern <febri.arianto95@gmail.com>
2021-04-29 12:15:16 +0700

    src project

---
+++ routes/route.go
@@ -0,0 +1,32 @@
+package routes
+
+import (
+
+	// "project/controllers"
+	mid "project/middleware"
+
+	"github.com/labstack/echo"
+)
+
+func New() *echo.Echo {
+	e := echo.New()
+	mid.LogMiddleware(e)
+	// e.POST("/users", controllers.CreateUsersController)
+	// e.POST("/login", controllers.LoginUsersController)
+	// e.GET("/product", controllers.GetProductController)
+	// // e.GET("/users", controllers.GetUsersController)
+
+	// jwt := e.Group("jwt")
+	// jwt.Use(middleware.JWT([]byte(constants.KEY_JWT)))
+	// jwt.GET("", controllers.GetUsersController)
+	// jwt.POST("/product", controllers.CreateProductController)
+	// jwt.GET("/product", controllers.GetProductController)
+	// jwt.POST("/cart", controllers.CreateCartController)
+	// jwt.GET("/cart", controllers.GetCartController)
+	// jwt.DELETE("/cart/:id", controllers.DeleteCartController)
+	// jwt.POST("/checkout", controllers.CreateTransactionController)
+
+	// e.GET("/users/companies", controllers.GetUsersCompanyController)
+	// e.POST("/users/companies", controllers.CreateUserCompaniesController)
+	return e
+}
