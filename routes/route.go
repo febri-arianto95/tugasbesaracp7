f2a9914fbdb7110781df4ffc5cdbf068c4a7af12
ern <febri.arianto95@gmail.com>
2021-04-29 17:11:15 +0700

    update registry config

--- routes/route.go
+++ routes/route.go
@@ -1,10 +1,12 @@
 package routes
 
 import (
+	"project/constants"
 	"project/controllers"
 	mid "project/middleware"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func New() *echo.Echo {
@@ -14,6 +16,7 @@ func New() *echo.Echo {
 	e.POST("/login", controllers.LoginUsersController)
 	e.GET("/product", controllers.GetProductController)
 	jwt := e.Group("")
+	jwt.Use(middleware.JWT([]byte(constants.KEY_JWT)))
 	jwt.POST("/product", controllers.CreateProductController)
 	jwt.POST("/cart", controllers.CreateCartController)
 	jwt.GET("/cart", controllers.GetCartController)
